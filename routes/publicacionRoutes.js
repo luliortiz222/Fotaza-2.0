ea33e61340489ea7d11414d5e2574a5f817424eb
Lourdes <luliortiz222@gmail.com>
2026-06-09 14:10:40 -0300

    capturar errores multer

--- routes/publicacionRoutes.js
+++ routes/publicacionRoutes.js
@@ -8,7 +8,22 @@ router.get('/crear', estaLogueado, publicacionController.mostrarFormulario);
 router.get('/', publicacionController.obtenerFeedGlobal);
 router.get('/mi-perfil', estaLogueado, publicacionController.obtenerPerfilPersonal);
 
-router.post('/crear', estaLogueado, upload.single('imagen'), publicacionController.crearPublicacion);
+router.post('/crear', estaLogueado, (req, res, next) => {
+
+  upload.single('imagen')(req, res, function(err) {
+
+    if (err) {
+      console.error('========== ERROR MULTER ==========');
+      console.error(err);
+      console.error('MENSAJE:', err.message);
+      return res.status(500).send(err.message);
+    }
+
+    next();
+  });
+
+}, publicacionController.crearPublicacion);
+
 router.post('/eliminar/:id', estaLogueado, publicacionController.eliminarPublicacion);
 
 module.exports = router;
